2e38260f5393b9748c5052fe2415dbf8db3e2bbb
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-12-05 19:08:57 -0600

    Add async function to get weather data

--- script.js
+++ script.js
@@ -6,3 +6,18 @@
     tomorrowName.textContent = dayNames[(today +1) % 7];
     dayAfterTomorrowName.textContent = dayNames[(today +2) % 7];
 })();
+
+async function getData(location = 'London') {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days%2Chours%2Ccurrent&key=3PA9H9QZZJQTPLF2FC4ZYDM6P&contentType=json`);
+        if (!response.ok) {
+            alert('Something went wrong. Please try again.');
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const allData = await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getData();
